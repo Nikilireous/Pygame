3eacd2840c18c613908069c9c9fcf1e489f3b954
Nikita Golubkov <golubkoffnickita0@gmail.com>
2025-01-12 23:55:24 +0300

    Исправлена ошибка выхода из игрового цикла

--- game/main_game.py
+++ game/main_game.py
@@ -164,7 +164,7 @@ def main_game(char: str | None, difficult='Hard'):
         pygame.display.flip()
 
     pygame.mixer.music.stop()
-    pygame.quit()
+    return False, interface.current_time
 
 
 if __name__ == "__main__":
